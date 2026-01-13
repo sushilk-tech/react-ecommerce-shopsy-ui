156e5da74104d6e6a9419fe394568bab328c56f5
SushilK-tech <sushilk.tech26@gmail.com>
2026-01-13 23:06:51 +0530

    Insert assest file folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Logo from "../assets/logo.png"
+
 
 const Navbar = () => {
   return (
     <div>
-        
+        {/* upper Navbar */}
+        <div>
+            <div>
+                <div>
+                    <a href='#'>
+                        <img src={Logo} alt='Logo' />
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        {/* lower Navbar */}
+        <div>
+
+        </div>
     </div>
   )
 }
